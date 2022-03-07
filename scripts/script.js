6b4c0ca8de8976e660ab3f8d4bf8d15eb260e94d
Camille Frantz <camille.frantz@me.com>
2022-03-07 02:10:49 -0600

    Both times formats showing.

--- scripts/script.js
+++ scripts/script.js
@@ -4,25 +4,27 @@ function showTime() {
 	let hour = time.getHours();
 	let min = time.getMinutes();
 	let sec = time.getSeconds();
-	am_pm = "AM";
+    am_pm = "AM";
+    let hour2 = hour;
 
 	if (hour > 12) {
-		hour -= 12;
+		hour2 -= 12;
 		am_pm = "PM";
 	}
 	if (hour == 0) {
-		hr = 12;
+		hour2 = 12;
 		am_pm = "AM";
 	}
 
 	hour = hour < 10 ? "0" + hour : hour;
-	min = min < 10 ? "0" + min : min;
+    min = min < 10 ? "0" + min : min;
 	sec = sec < 10 ? "0" + sec : sec;
 
-	let currentTime = hour + ":"
-			+ min + ":" + sec + am_pm;
+	let currentTime = hour + ":" + min + ":" + sec;
+    let ampmTime = hour2 + ":" + min + ":" + sec + am_pm;
 
-	document.getElementById("clock")
-			.innerHTML = currentTime;
+	document.getElementById("clock").innerHTML = currentTime + "\n" + ampmTime;
+    // document.querySelector("ampmtime").innerHTML = ampmTime;
+
 }
 showTime();
